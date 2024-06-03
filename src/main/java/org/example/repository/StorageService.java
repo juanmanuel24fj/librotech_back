1d0560bda0e988931028915d4b56cf297b4717ae
juanmanuel24fj <juanmanuel2004fj@gmail.com>
2024-06-03 10:33:03 +0200

    Update StorageService.java

--- src/main/java/org/example/repository/StorageService.java
+++ src/main/java/org/example/repository/StorageService.java
@@ -1,11 +1,12 @@
 package org.example.repository;
 
 import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-
+@Repository
 public interface StorageService {
     void init() throws IOException;
     String store(MultipartFile file);
