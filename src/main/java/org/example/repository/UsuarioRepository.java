a3a816d83885a39ec082121c919465e69d115ae1
juanmanuel24fj <juanmanuel2004fj@gmail.com>
2024-06-03 10:32:41 +0200

    Update UsuarioRepository.java

--- src/main/java/org/example/repository/UsuarioRepository.java
+++ src/main/java/org/example/repository/UsuarioRepository.java
@@ -1,14 +1,13 @@
-
 package org.example.repository;
 
 import org.example.model.Reserva;
 import org.example.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByUsername(String username);
 
 }
-
