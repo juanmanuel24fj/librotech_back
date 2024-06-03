095f39bc764fa064205902c0d6c6f311de031786
juanmanuel24fj <juanmanuel2004fj@gmail.com>
2024-06-03 10:33:47 +0200

    Update LibroRepository.java

--- src/main/java/org/example/repository/LibroRepository.java
+++ src/main/java/org/example/repository/LibroRepository.java
@@ -2,9 +2,10 @@ package org.example.repository;
 
 import org.example.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface LibroRepository extends JpaRepository<Libro, Long> {
 
      Libro findByTitulo(String titulo);
