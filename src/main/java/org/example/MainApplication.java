9e6760d9233ad282b572884f058819e71ea3e356
juanmanuel24fj <juanmanuel2004fj@gmail.com>
2024-06-03 10:53:36 +0200

    Update MainApplication.java

--- src/main/java/org/example/MainApplication.java
+++ src/main/java/org/example/MainApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "org.example.repository")
-@EntityScan(basePackages = "org.example.model")
 public class MainApplication {
 
     public static void main(String[] args) {
