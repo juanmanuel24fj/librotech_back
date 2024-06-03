3b58a53cf85a534603318f6789dc4b315d336b69
juanmanuel24fj <juanmanuel2004fj@gmail.com>
2024-06-03 10:33:26 +0200

    Update ReservaRepository.java

--- src/main/java/org/example/repository/ReservaRepository.java
+++ src/main/java/org/example/repository/ReservaRepository.java
@@ -2,10 +2,11 @@ package org.example.repository;
 
 import org.example.model.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
-
+@Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
     List<Reserva> findAllByFechaDevolucionBefore(LocalDate fecha);
     List<Reserva> findAllByUsuarioId(Long usuarioId);
